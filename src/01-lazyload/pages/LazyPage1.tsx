f0af67877619bf821484948296bb706f08b05c95
root <root@DESKTOP-NQDBL4F>
2024-08-18 13:14:51 -0500

    Lazy added

---
+++ src/01-lazyload/pages/LazyPage1.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LazyPage1 = () => {
+  return (
+    <div>LazyPage1</div>
+  )
+}
+
+export default LazyPage1
---
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LazyPage3 = () => {
+  return (
+    <div>LazyPage3</div>
+  )
+}
+
+export default LazyPage3
